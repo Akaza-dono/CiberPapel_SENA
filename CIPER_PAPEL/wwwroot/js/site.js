9b6fdd2d54b89424f3aa8ce267356307f2b2e22a
alejandro <alejoquevedo0598@hotmail.com>
2024-05-22 20:31:38 -0500

    cambios de estilos

--- CIPER_PAPEL/wwwroot/js/site.js
+++ CIPER_PAPEL/wwwroot/js/site.js
@@ -1,10 +1,4 @@
-﻿//const panel = document.getElementById('options-menu');
-//panel.addEventListener('click', () => {
-//    panel.style.gridColumn = '1 / span 2'; // Esto hará que la columna ocupe dos fracciones
-//    console.log('asdas')
-//});
-
-var wi = '0.2'
+﻿var wi = '0.2'
 
 const elementContainer = document.querySelector('.element-container');
 elementContainer.style.border = '0px solid black';
@@ -13,14 +7,18 @@ elementContainer.style.width = '100%';
 elementContainer.style.borderRadius = '4px';
 elementContainer.style.display = 'grid';
 elementContainer.style.gridTemplateColumns = `${wi}fr repeat(5, 1fr)`;
+
 elementContainer.style.gridTemplateRows = '0.3fr repeat(5, 1fr)';
+
 
 const optionsMenu = document.getElementById('options-menu');
 
 
 
 optionsMenu.addEventListener('mouseover', () => {
+    
     elementContainer.style.gridTemplateColumns = `0.7fr repeat(5, 1fr)`;
+    elementContainer.style.animationDuration = '3s';
 })
 
 optionsMenu.addEventListener('mouseleave', () => {
