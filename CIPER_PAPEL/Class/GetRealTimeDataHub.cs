c5f6569b4edc3bfbf2cf5c10bd1b43720dae4207
Macosta <vacacionesvillavicencio@gmail.com>
2024-05-28 21:44:44 -0500

    Se crea HUB para cambios en tiempo real

---
+++ CIPER_PAPEL/Class/GetRealTimeDataHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CIPER_PAPEL.Class
+{
+    public class GetRealTimeDataHub : Hub
+    {
+        public async Task Send(int total = 45)
+        {
+            await Clients.All.SendAsync("Recieve", total);
+        }
+    }
+}
